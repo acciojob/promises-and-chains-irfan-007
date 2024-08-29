effc7b675c4d0746c8401111bad95fbe35e6b625
irfan <85334710+irfan-007@users.noreply.github.com>
2024-08-29 19:45:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const name=document.getElementById("name");
+const age=document.getElementById("age");
+const btn=document.getElementById("btn");
+
+btn.addEventListener("click",async (e)=>{
+	e.preventDefault();
+	verifyAge()
+		.then((msg)=>alert(msg))
+		.catch((error)=>alert(error));
+});
+
+function verifyAge() {
+	return new Promise((res,rej)=>{
+		setTimeout(()=>{
+			if(age.value>18)
+				res(`Welcome, ${name.value}. You can vote.`);
+			else
+				rej(`Oh sorry ${name.value}. You aren't old enough.`);
+		},4000);
+	});
+}
